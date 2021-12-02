49b91bdaf7335153ea1f8d3aec2dee5960a2854d
Karl Xu <karl.yogi.xu@gmail.com>
2021-12-02 15:13:47 +0800

    debug: print out claim type and value

--- src/shortenerTools/Domain/Utility.cs
+++ src/shortenerTools/Domain/Utility.cs
@@ -97,7 +97,7 @@ namespace Cloud5mins.domain
             foreach(Claim claim in principal.Claims)
             {
                 log.LogInformation("2");
-                // log.LogInformation("CLAIM TYPE: " + claim.Type + "; CLAIM VALUE: " + claim.Value)
+                log.LogInformation("CLAIM TYPE: " + claim.Type + "; CLAIM VALUE: " + claim.Value)
             }
             /* Debug code END */
             if (principal.FindFirst(ClaimTypes.Name) is null)
